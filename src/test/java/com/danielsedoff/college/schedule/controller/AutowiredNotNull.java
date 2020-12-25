127c8dd42b6f678ab4f59c2f4d21ffd170ca2a9c
Daniel Sedoff <danielsedoff@gmail.com>
2020-12-25 03:24:30 +0300

    Trying to fix the Application Context issues

--- src/test/java/com/danielsedoff/college/schedule/controller/AutowiredNotNull.java
+++ src/test/java/com/danielsedoff/college/schedule/controller/AutowiredNotNull.java
@@ -12,7 +12,6 @@ import org.springframework.test.context.ContextConfiguration;
 
 import com.danielsedoff.college.schedule.config.AppConfig;
 
-
 @ContextConfiguration(classes = AppConfig.class)
 public class AutowiredNotNull {
     @Autowired
