6721c76e84cceb2227afd202ba11db12268c6e7d
Daniel Sedoff <danielsedoff@gmail.com>
2020-12-25 03:24:56 +0300

    Adding Swagger 2

--- swagger/run-swagger.bat
+++
@@ -1,9 +0,0 @@
-set swaggerpath=c:\software\swagger\swagger-codegen-cli-2.2.1.jar
-set apidocspath=http://localhost:8080/v2/api-docs
-
-md swagger
-cd swagger
-
-java -jar %swaggerpath% generate -i %apidocspath% -l java
-
-pause
