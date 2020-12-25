766fdee8c351362e1c43e7825c7d2136483f0727
Daniel Sedoff <danielsedoff@gmail.com>
2020-12-25 03:24:29 +0300

    Adding the StudentStatus servlet

---
+++ src/main/java/com/danielsedoff/college/schedule/servlet/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.danielsedoff.college.schedule.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "MyServlet", urlPatterns = { "/hello" })
+public class HelloServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 3837865670959058152L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("this is HelloServlet running.".getBytes());
+        out.flush();
+        out.close();
+    }
+
+}
