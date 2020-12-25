b7f7ca6ea278a2d6e4a184fea1f5aa19ebec8fd6
Daniel Sedoff <danielsedoff@gmail.com>
2020-12-25 03:24:55 +0300

    Merge branch dev into master

---
+++ src/main/java/com/danielsedoff/college/schedule/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.danielsedoff.college.schedule.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer {
+    @Bean
+    public Docket coursesApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.danielsedoff.college.schedule.controller"))
+                .paths(PathSelectors.regex("/.*")).build().apiInfo(metaData());
+
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder().title("Spring Boot REST API")
+                .description("\"Spring Boot REST API for the College Schedule Web App\"").version("0.1")
+
+                .license("GNU GPL 2.0").licenseUrl("https://www.gnu.org/licenses/old-licenses/gpl-2.0.html")
+                .contact(new Contact("Daniel Sedoff", "https://github.com/danielsedoff", "sedoff@zohomail.com"))
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
--- src/test/java/com/danielsedoff/college/schedule/controller/rest/ProfessorRestControllerTest.java
+++ src/test/java/com/danielsedoff/college/schedule/controller/rest/ProfessorRestControllerTest.java
@@ -7,11 +7,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.io.IOException;
-import java.sql.SQLException;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,8 +17,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import com.danielsedoff.college.schedule.repositories.SqlScriptRunner;
 
 @SpringBootTest
 @AutoConfigureMockMvc
