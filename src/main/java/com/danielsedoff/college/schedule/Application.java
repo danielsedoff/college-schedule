e9b8b601bd1efc7b9b0d9ccfb8e62a8a4445d6a4
Daniel Sedoff <danielsedoff@gmail.com>
2020-12-25 03:24:48 +0300

    Adding Spring Boot configuration...

--- src/main/java/com/danielsedoff/college/schedule/Application.java
+++ src/main/java/com/danielsedoff/college/schedule/Application.java
@@ -2,16 +2,21 @@ package com.danielsedoff.college.schedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
-import com.danielsedoff.college.schedule.config.MainWebAppInitializer;
 import com.danielsedoff.college.schedule.config.WebConfig;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.danielsedoff.college.schedule")
-@Import({ WebConfig.class, MainWebAppInitializer.class })
-public class Application {
+@Import(WebConfig.class)
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
