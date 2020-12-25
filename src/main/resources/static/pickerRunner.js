584b5ae84ea0205d35ee3b3ee624215344e27834
Daniel Sedoff <danielsedoff@gmail.com>
2020-12-25 03:24:38 +0300

    Initial commit

--- src/main/resources/static/pickerRunner.js
+++
@@ -1,7 +0,0 @@
-function pickerRunner(inputId) {
-    var input = document.getElementById(inputId);
-    var picker = new Picker(input, {
-        format: 'YYYY-MM-DD HH:mm',
-    });
-    picker.show();
-}
