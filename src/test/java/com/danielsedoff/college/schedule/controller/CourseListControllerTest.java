ff88de680d6d4c456e42c2381b3e6c6adf554384
Daniel Sedoff <danielsedoff@gmail.com>
2020-12-25 03:24:34 +0300

    Rewriting CourseListControllerTest as an integration test

--- src/test/java/com/danielsedoff/college/schedule/controller/CourseListControllerTest.java
+++ src/test/java/com/danielsedoff/college/schedule/controller/CourseListControllerTest.java
@@ -1,43 +1,53 @@
 package com.danielsedoff.college.schedule.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.List;
 
 import javax.servlet.ServletContext;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.danielsedoff.college.schedule.config.WebConfig;
 import com.danielsedoff.college.schedule.dao.CourseDAO;
+import com.danielsedoff.college.schedule.model.Course;
 import com.danielsedoff.college.schedule.service.CourseService;
 
-@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = { WebConfig.class, NameServiceTestConfiguration.class })
+@RunWith(JUnitPlatform.class)
+@ContextConfiguration(classes = WebConfig.class)
+
 @WebAppConfiguration
 public class CourseListControllerTest {
     @Mock
-    CourseService courseService;
-
-    @Mock
     CourseDAO coursedao;
+    
+    @Mock
+    CourseService courseService;
 
     @Autowired
     private WebApplicationContext wac;
@@ -45,7 +55,8 @@ public class CourseListControllerTest {
     private MockMvc mockMvc;
 
     @BeforeEach
-    public void setup() throws Exception {
+    public void mockInitializer() throws Exception {
+        MockitoAnnotations.initMocks(this);
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
@@ -60,12 +71,26 @@ public class CourseListControllerTest {
 
     @Test
     public void givenCourseURI_whenMockMVC_thenReturnsCourseViewName() throws Exception {
-        mockMvc.perform(get("/courses")).andDo(print()).andExpect(view().name("courses"));
+        Course course = new Course();
+        course.setId(1);
+        course.setName("Algebra");
+        List<Integer> ids = List.of(1, 2, 3);
+        Mockito.when(courseService.getCourseIdList()).thenReturn(ids);
+        Mockito.when(courseService.getCourseById(Mockito.anyInt())).thenReturn(course);
+
+        Mockito.when(coursedao.getIdList()).thenReturn(ids);
+        
+        ResultActions performResult = mockMvc.perform(get("/courses"));
+        ResultActions resultPrint = performResult.andDo(print());
+        resultPrint.andExpect(view().name("courses"));
     }
 
     @Test
     public void givenCourseURI_whenMockMVC_thenVerifyResponse() throws Exception {
-        mockMvc.perform(get("/courses")).andExpect(status().isOk())
-                .andExpect(model().attributeExists("testvalue"));
+        MvcResult mvcResult = this.mockMvc.perform(get("/courses")).andDo(print())
+                .andExpect(status().isOk()).andExpect(jsonPath("$.testvalue").value("passed"))
+                .andReturn();
+
+        assertEquals("application/json;charset=UTF-8", mvcResult.getResponse().getContentType());
     }
 }
