8fc07a89b087d16f300304aaafea7883a804ef99
Daniel Sedoff <danielsedoff@gmail.com>
2020-12-25 03:24:40 +0300

    Changed Hibernate Dialect.

--- src/main/java/com/danielsedoff/college/schedule/dao/StudentDAO.java
+++ src/main/java/com/danielsedoff/college/schedule/dao/StudentDAO.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +18,9 @@ public class StudentDAO implements DAO<Student> {
 
     @Autowired
     EntityManagerConfig emf;
+    
+    private static Logger logger = LoggerFactory.getLogger(StudentDAO.class);
+
 
     public List<Integer> getIdList() throws DAOException {
         List<Integer> result = new ArrayList<>();
@@ -81,12 +86,10 @@ public class StudentDAO implements DAO<Student> {
             EntityManager em = emf.getFactory().createEntityManager();
             em.getTransaction().begin();
             em.persist(student);
-            
-            em.persist(student);
-            em.getTransaction().commit();
-            
             em.close();
         } catch (Exception e) {
+            e.printStackTrace();// DEBUG if logger failed
+            logger.error(e.getStackTrace().toString());
             throw new DAOException("Could not create Student", e);
         }
         return result;
