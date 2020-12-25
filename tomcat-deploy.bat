e6a7148d85e3df839e79cc10d966e2de97f34e03
Daniel Sedoff <danielsedoff@gmail.com>
2020-12-25 03:24:29 +0300

    Having fun with Tomcat or not that much fun for that matter 2

--- tomcat-deploy.bat
+++
@@ -1,8 +0,0 @@
-rem Copy a .war file to tomcat's public directory
-
-set warfile=college-schedule-0.0.1-SNAPSHOT.war
-set tompub=C:\tomcat\apache-tomcat-9.0.38\webapps
-set build=C:\javaprojects\college-schedule\target
-
-if exist %tompub%\%warfile% del %tompub%\%warfile%
-copy %build%\%warfile% %tompub%
