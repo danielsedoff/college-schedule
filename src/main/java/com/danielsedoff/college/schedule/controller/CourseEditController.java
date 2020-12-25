53799ecba3aaf83b7f2bf34ac6fcf8a28a1456ba
Daniel Sedoff <danielsedoff@gmail.com>
2020-12-25 03:24:36 +0300

    The first CRUD controller is working.

--- src/main/java/com/danielsedoff/college/schedule/controller/CourseEditController.java
+++ src/main/java/com/danielsedoff/college/schedule/controller/CourseEditController.java
@@ -13,25 +13,79 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.danielsedoff.college.schedule.model.Course;
+import com.danielsedoff.college.schedule.model.Professor;
 import com.danielsedoff.college.schedule.service.CourseService;
 
 @Controller
 public class CourseEditController {
 
-    private static final String DELIMITER = "; ";
     @Autowired
     private CourseService cs;
 
+    String getParam(String haystack, String needle) {
+        int startPosition = haystack.indexOf(needle + "=");
+        if (startPosition == -1) {
+            return null;
+        }
+        int endPosition = haystack.indexOf("&", startPosition);
+        if ((endPosition == -1) || (endPosition - startPosition < 1)) {
+            return null;
+        }
+        String result = haystack.substring(startPosition + needle.length() + 1, endPosition);
+        return result;
+    }
+
     @PostMapping("/courseEdit")
     @ResponseBody
-    public String courseEditStub(HttpServletRequest request,
-            HttpServletResponse response, Model model) {
-        String mode = request.getParameter("mode");
-        String courseid = request.getParameter("courseid");
-        String coursedescription = request.getParameter("coursedescription");
-        String professorid = request.getParameter("professorid");
+    public String editCourses(HttpServletRequest request, HttpServletResponse response,
+            Model model) {
 
-        return (mode + DELIMITER + courseid + DELIMITER + coursedescription + DELIMITER + professorid); 
+        String userRequest = request.getParameter("encoded");
+        /* DEBUG */ System.out.println("userRequest: " + userRequest);
+        String mode = getParam(userRequest, "mode");
+        String name = getParam(userRequest, "name");
+        String idInput = getParam(userRequest, "idinput");
+        String description = getParam(userRequest, "description");
+        String connectedId1 = getParam(userRequest, "connectedid1");
+
+        if (mode.equals("create")) {
+
+            List<Professor> professors = new ArrayList<Professor>();
+            Professor prof = new Professor();
+            prof.setId(Integer.parseInt(connectedId1));
+            professors.add(prof);
+
+            Course course = new Course();
+            course.setProfessors(professors);
+            course.setCourseDescription(description);
+            course.setName(name);
+            cs.createCourse(course);
+            return "Your CREATE request has been accepted by the server.";
+        }
+
+        if (mode.equals("update")) {
+
+            List<Professor> professors = new ArrayList<Professor>();
+            Professor prof = new Professor();
+            prof.setId(Integer.parseInt(connectedId1));
+            professors.add(prof);
+
+            Course course = new Course();
+            course.setId(Integer.parseInt(idInput));
+            course.setProfessors(professors);
+            course.setCourseDescription(description);
+            course.setName(name);
+            cs.updateCourse(Integer.parseInt(idInput), course);
+            return "Your UPDATE request has been accepted by the server.";
+        }
+
+        if (mode.equals("delete")) {
+
+            cs.deleteCourseById(Integer.parseInt(idInput));
+            return "Your DELETE request has been accepted by the server.";
+        }
+
+        return "The server has recieved an unsupported mode command: " + mode;
     }
 
     public String getCourses(Model model) {
--- src/main/resources/static/radios.js
+++ src/main/resources/static/radios.js
@@ -1,15 +1,38 @@
-radCreate = document.getElementById("radioCreate");
-radDelete = document.getElementById("radioDelete");
-radUpdate = document.getElementById("radioUpdate");
+radioCreate = document.getElementById("radioCreate");
+radioDelete = document.getElementById("radioDelete");
+radioUpdate = document.getElementById("radioUpdate");
 
-radCreate.addEventListener('change', function(){
-    alert('create: ' + this.value);
-})
+radioCreate.addEventListener('change', radioChanged);
+radioDelete.addEventListener('change', radioChanged);
+radioUpdate.addEventListener('change', radioChanged);
 
-radDelete.addEventListener('change', function(){
-    alert('delete: ' + this.value);
-})
+function radioChanged() {
+    if (document.getElementById("radioCreate").checked) {
+        radioValue = "create";
+    } else if (document.getElementById("radioDelete").checked) {
+        radioValue = "delete";
+    } else if (document.getElementById("radioUpdate").checked) {
+        radioValue = "update";
+    }
 
-radUpdate.addEventListener('change', function(){
-    alert('update: ' + this.value);
-})
+    var inputs = document.getElementsByTagName('input');
+
+    for (var i = 0; i < inputs.length; i++) {
+        input = inputs[i];
+        if (input.type != "text") continue;
+        if (input.id == "idinput") {
+            if (radioValue != "create") {
+                input.disabled = false;
+            } else {
+                input.disabled = true;
+            }
+        }
+        if (input.id != "idinput") {
+            if (radioValue == "delete") {
+                input.disabled = true;
+            } else {
+                input.disabled = false;
+            }
+        }
+    }
+}
