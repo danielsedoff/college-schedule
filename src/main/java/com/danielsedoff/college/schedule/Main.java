3431ebb3961a787bcdac66e7544e71baca066162
Daniel Sedoff <danielsedoff@gmail.com>
2020-12-25 03:24:21 +0300

    Delete ControllerAndView.xml

--- src/main/java/com/danielsedoff/college/schedule/Main.java
+++
@@ -1,7 +0,0 @@
-package com.danielsedoff.college.schedule;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Method main() of class Main running");
-    }
-}
