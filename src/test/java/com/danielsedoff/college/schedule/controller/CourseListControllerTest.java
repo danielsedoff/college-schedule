c2f2b2d5068121d7ee6f7397ddf2ae95aa9989d5
Daniel Sedoff <danielsedoff@gmail.com>
2020-12-25 03:24:34 +0300

    CourseListControllerTest works

--- src/test/java/com/danielsedoff/college/schedule/controller/CourseListControllerTest.java
+++ src/test/java/com/danielsedoff/college/schedule/controller/CourseListControllerTest.java
@@ -1,11 +1,10 @@
 package com.danielsedoff.college.schedule.controller;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -14,6 +13,7 @@ import javax.servlet.ServletContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.ActiveProfiles;
@@ -21,22 +21,22 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.danielsedoff.college.schedule.config.WebConfig;
 import com.danielsedoff.college.schedule.dao.CourseDAO;
 import com.danielsedoff.college.schedule.service.CourseService;
 
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {  NameServiceTestConfiguration.class })
+@ContextConfiguration(classes = { WebConfig.class, NameServiceTestConfiguration.class })
 @WebAppConfiguration
 public class CourseListControllerTest {
-    @Autowired
+    @Mock
     CourseService courseService;
 
-    @Autowired
+    @Mock
     CourseDAO coursedao;
 
     @Autowired
@@ -60,15 +60,12 @@ public class CourseListControllerTest {
 
     @Test
     public void givenCourseURI_whenMockMVC_thenReturnsCourseViewName() throws Exception {
-        this.mockMvc.perform(get("/courses")).andDo(print()).andExpect(view().name("courses"));
+        mockMvc.perform(get("/courses")).andDo(print()).andExpect(view().name("courses"));
     }
 
     @Test
     public void givenCourseURI_whenMockMVC_thenVerifyResponse() throws Exception {
-        MvcResult mvcResult = this.mockMvc.perform(get("/courses")).andDo(print())
-                .andExpect(status().isOk()).andExpect(jsonPath("$.testvalue").value("passed"))
-                .andReturn();
-
-        assertEquals("application/json;charset=UTF-8", mvcResult.getResponse().getContentType());
+        mockMvc.perform(get("/courses")).andExpect(status().isOk())
+                .andExpect(model().attributeExists("testvalue"));
     }
 }
